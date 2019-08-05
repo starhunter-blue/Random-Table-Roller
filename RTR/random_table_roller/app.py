8b09167cf2cbe6b688449e1bf3f4bdacd4816455
starhunter-blue <42650006+starhunter-blue@users.noreply.github.com>
2019-08-05 15:06:32 +0200

    Test Gui

--- RTR/random_table_roller/app.py
+++ RTR/random_table_roller/app.py
@@ -1,16 +1,14 @@
 import PySimpleGUI as sg
+import sys
 
 def run():
-    layout = [      
-          [sg.Text('Please enter your Name, Address, Phone')],      
-          [sg.Text('Name', size=(15, 1)), sg.InputText('name')],      
-          [sg.Text('Address', size=(15, 1)), sg.InputText('address')],      
-          [sg.Text('Phone', size=(15, 1)), sg.InputText('phone')],      
-          [sg.Submit(), sg.Cancel()],      
-          [sg.Text("Halluuuuuuuuuuuuuuuu")]
-    ]
+      if len(sys.argv) == 1:
+            fname = sg.PopupGetFile(message = "Open Random Table", file_types= (("All Files", "*.*"), ("Text Files", ".txt")))
+      else:
+            fname = sys.argv[1]
 
-    window = sg.Window('Simple data entry window').Layout(layout)         
-    button, values = window.Read()
-
-    print(button, values[0], values[1], values[2])
+      if not fname:
+            sg.Popup("Cancel", "No filename supplied")
+            raise SystemExit("Cancelling: no filename supplied")
+      else:
+            sg.Popup('The filename you chose was', fname)
