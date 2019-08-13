496ac99015b3107822235756978418a6b43f92fd
starhunter-blue <42650006+starhunter-blue@users.noreply.github.com>
2019-08-13 18:08:08 +0200

    Table can create dict
    Both from single and range entries

--- RTR/random_table_roller/common/table.py
+++ RTR/random_table_roller/common/table.py
@@ -3,16 +3,21 @@ from exceptions import InvalidEntryNumberingException, DuplicateEntryException
 
 ENTRY_NUMBERING_VALID_REGEX = re.compile(r"(\d+(-\d+)*:)")
 ENTRY_SINGLE_NUMBER_REGEX = re.compile(r"^(\d+):(.*)")
-ENTRY_RANGE_REGEX = re.compile(r"\d+-\d+:")
+ENTRY_RANGE_REGEX = re.compile(r"^(\d+-\d+):(.*)")
 
 def create_table(name, entries):
     results = {}
 
-    def add_single_entry(single_entry):
-        entry_nr = int(single_entry.group(1))
-        entry_value = single_entry.group(2)
+    def add_entry(entry_nr, entry_value):
         check_if_duplicate_entry(entry_nr)
         results[entry_nr] = entry_value
+
+    def add_range_entry(range_entry):
+        entry_range_str = range_entry.group(1).split("-")
+        entry_range = range(int(entry_range_str[0]), int(entry_range_str[1]) + 1)
+        entry_value = range_entry.group(2)
+        for i in entry_range:
+            add_entry(i, entry_value)
 
     def check_if_duplicate_entry(entry_nr):
         if entry_nr in results:
@@ -23,16 +28,17 @@ def create_table(name, entries):
             raise InvalidEntryNumberingException(name + " : " + entry)
 
         single_entry = re.search(ENTRY_SINGLE_NUMBER_REGEX, entry)
-        range_entry_nr = re.match(ENTRY_RANGE_REGEX, entry)
-        if single_entry is not None:
-            try:
-                add_single_entry(single_entry)
-            except DuplicateEntryException as e:
-                raise DuplicateEntryException("Numbers used by this entry are already in use: " + entry).with_traceback(e.__traceback__)
-        
-        elif range_entry_nr is not None:
-            pass
-            #print("Range: " + range_entry_nr.group())
+        range_entry = re.search(ENTRY_RANGE_REGEX, entry)
+        try:
+            if single_entry is not None:
+                    entry_nr = int(single_entry.group(1))
+                    entry_value = single_entry.group(2)
+                    add_entry(entry_nr, entry_value)
+            elif range_entry is not None:
+                add_range_entry(range_entry)
+        except DuplicateEntryException as e:
+            raise DuplicateEntryException("Numbers used by this entry are already in use: " + entry).with_traceback(e.__traceback__)
+    
 
         print(results)
     
@@ -55,5 +61,5 @@ class ComplexTable(Table):
     def __init__(self, name):
         super().__init__(self, name)
 
-entries = ["1: 1-4:", "2: Test", "3-10: Test"]
+entries = ["1: 1-4:", "2: Test", "3-6: Test"]
 create_table("Bla", entries)
